573759f455eb1c72ced1c215a00006f3e5412512
dt2 <derek.thomas2@target.com>
2021-01-28 10:13:53 -0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # PyProofpoint
 Python Wrapper for Proofpoint's Threat Insight API
+
+## Install
+```pip3 install pyproofpoint```
+
+## Usage
+```
+from pyproofpoint import proofpoint
+from datetime import date
+
+servicePrincipal = "Service Account"
+APISecret = "Service AccountSecret"
+
+pp = proofpoint.ProofPoint(servicePrincipal, APISecret)
+
+my_date = date(2020, 12, 12).isoformat()
+interval = "PT12H/" + my_date
+
+campaigns = pp.get_campaign_ids(interval=interval)
+print(campaigns)
+```
---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyproofpoint",
+    version="0.0.1",
+    author="Derek Thomas2",
+    author_email="derek.e.thomas@biola.edu",
+    maintainer='Derek Thomas2',
+    description="A python wrapper for proofpoint's threat insight API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Derekt2/PyProofpoint",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Telecommunications Industry',
+        'Intended Audience :: Information Technology',
+        'Topic :: Security',
+        'Topic :: Internet',
+    ],
+    python_requires='>=3.5',
+    install_requires=[
+   'requests'
+   ]
+)
